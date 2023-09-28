0ba3ca9863bea17200fad459a62c0097c4eaf541
Dmitry48 <Dima_1996.33@mail.ru>
2023-09-28 16:39:08 +0700

    HW3

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -15,9 +15,7 @@ export const HW3 = () => {
     'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
   ]);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setCurrentText(currentText);
-  };
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => setCurrentText(event.currentTarget.value);
 
   const handleSave = () => {
       setTexts([...texts, currentText]);
@@ -29,9 +27,9 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-        <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
-        <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
+        <h1 id={'hw03-default-text'}>Здеесь появится ново дело</h1> // ничего не меняем, здесь все норм
       )}
 
       <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
